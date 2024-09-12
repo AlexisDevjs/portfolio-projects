20bddc574b32e8398270e457c027cfc96340da5c
AlexisDevjs <alexis.devct@gmail.com>
2024-09-11 22:20:52 -0500

    chore: add and configure eslint and prettier

--- JavaScript-Quiz/eslint.config.js
+++ JavaScript-Quiz/eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import stylistic from '@stylistic/eslint-plugin'
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -11,18 +12,28 @@ export default tseslint.config(
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: globals.browser
     },
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      '@stylistic': stylistic
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
-        { allowConstantExport: true },
+        { allowConstantExport: true }
       ],
-    },
-  },
+      '@typescript-eslint/consistent-type-imports': 'error',
+      '@stylistic/space-before-function-paren': 'error',
+      '@stylistic/jsx-one-expression-per-line': 'off',
+      '@stylistic/arrow-parens': 'off',
+      '@stylistic/eol-last': 'error',
+      '@stylistic/comma-dangle': 'error',
+      '@stylistic/jsx-quotes': ['error', 'prefer-single'],
+      '@stylistic/semi': ['error', 'never'],
+      '@stylistic/no-trailing-spaces': 'error'
+    }
+  }
 )
